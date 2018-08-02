e9d57966b57ff6180d46b9e7dedcdb9bc66ec3b5
Marchin <mmonterrosa94@gmail.com>
2018-08-02 19:38:41 -0300

    Add Shot_test

--- EngineTester/Shot_test.cpp
+++ EngineTester/Shot_test.cpp
@@ -9,7 +9,23 @@ namespace EngineTester {
 public:
 
 	TEST_METHOD(Instatiation) {
+		shotSys::Shot shot;
+		Assert::AreEqual(shot.Width(), 8.f);
+		Assert::AreEqual(shot.Height(), 8.f);
+		Assert::AreEqual(shot.IsAvailable(), true);
+		Assert::AreEqual(shot.GetPosition().x, 0.f);
+		Assert::AreEqual(shot.GetPosition().y, 0.f);
+		Assert::AreEqual((char)shot.GetColor().r, (char)0);
+		Assert::AreEqual((char)shot.GetColor().g, (char)0);
+		Assert::AreEqual((char)shot.GetColor().b, (char)255);
+		Assert::AreEqual((char)shot.GetColor().a, (char)0);
+	}
 
+	TEST_METHOD(Positioning) {
+		shotSys::Shot shot;
+		shot.SetPosition(sf::Vector2f(3.f, 4.f));
+		Assert::AreEqual(shot.GetPosition().x, 3.f);
+		Assert::AreEqual(shot.GetPosition().y, 4.f);
 	}
 
 	};

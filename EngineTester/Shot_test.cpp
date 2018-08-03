aa3af35287d776beb6a4178b97347b789b015c96
Marchin <mmonterrosa94@gmail.com>
2018-08-03 11:18:04 -0300

    Add tests to Shot_test

--- EngineTester/Shot_test.cpp
+++ EngineTester/Shot_test.cpp
@@ -15,6 +15,7 @@ public:
 		Assert::AreEqual(shot.IsAvailable(), true);
 		Assert::AreEqual(shot.GetPosition().x, 0.f);
 		Assert::AreEqual(shot.GetPosition().y, 0.f);
+		Assert::AreEqual(shot.GetDirection(), 0.f);
 		Assert::AreEqual((char)shot.GetColor().r, (char)0);
 		Assert::AreEqual((char)shot.GetColor().g, (char)0);
 		Assert::AreEqual((char)shot.GetColor().b, (char)255);
@@ -24,8 +25,30 @@ public:
 	TEST_METHOD(Positioning) {
 		shotSys::Shot shot;
 		shot.SetPosition(sf::Vector2f(3.f, 4.f));
+		shot.SetDirection(45.f);
 		Assert::AreEqual(shot.GetPosition().x, 3.f);
 		Assert::AreEqual(shot.GetPosition().y, 4.f);
+		Assert::AreEqual(shot.GetDirection(), 45.f);
+	}
+
+	TEST_METHOD(Coloring) {
+		shotSys::Shot shot;
+		shot.SetColor(sf::Color::Red);
+		Assert::AreEqual((char)shot.GetColor().r, (char)255);
+		Assert::AreEqual((char)shot.GetColor().g, (char)0);
+		Assert::AreEqual((char)shot.GetColor().b, (char)0);
+		Assert::AreEqual((char)shot.GetColor().a, (char)255);
+	}
+
+	TEST_METHOD(Activation) {
+		shotSys::Shot shot;
+		shot.Enable();
+		Assert::AreEqual(shot.IsAvailable(), false);
+		shot.SetColor(color::Opaque(shot.GetColor()));
+		Assert::AreEqual((char)shot.GetColor().a, (char)255);
+		shot.Disable();
+		Assert::AreEqual(shot.IsAvailable(), true);
+		Assert::AreEqual((char)shot.GetColor().a, (char)0);
 	}
 
 	};

8f8d29706f85e962b657d6f1952fe70624f95ddc
Marchin <mmonterrosa94@gmail.com>
2018-08-03 16:44:28 -0300

    Add Input_test

---
+++ EngineTester/Input_test.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../SFML_Practica/Headers/Shot/ShotPool.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace EngineTester {
+	TEST_CLASS(ShotPool_test) {
+public:
+
+	TEST_METHOD(Instatiation) {
+		float deltaTime = 0.f;
+		std::vector<sf::Sprite> obstacles;
+		sf::View view(sf::FloatRect(0, 0, 256, 240));
+		shotSys::ShotPool shotPool(&view, 3);
+		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
+		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
+		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
+		Assert::IsFalse(shotPool.RequestShot(10, sf::Color::Red));
+		while (deltaTime < 5.f) {
+			shotPool.Update(deltaTime, obstacles);
+			deltaTime += .33f;
+		}
+		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
+		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
+		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
+		Assert::IsFalse(shotPool.RequestShot(10, sf::Color::Red));
+	}
+
+
+	};
+}
--- SFML_Practica/Headers/Controllers/Input.h
+++ SFML_Practica/Headers/Controllers/Input.h
@@ -6,12 +6,12 @@
 namespace input {
 	class Input {
 	public:
+		Input(sf::Keyboard::Key key = sf::Keyboard::F13) : m_key(key),
+			m_currState(false), m_prevState(false) {}
 		void Update();
 		bool IsPressed();
 		bool PressDown();
 		bool PressUp();
-		Input(sf::Keyboard::Key key = sf::Keyboard::F13) : m_key(key),
-			m_currState(false), m_prevState(false) {};
 	private:
 		sf::Keyboard::Key m_key;
 		bool m_currState;

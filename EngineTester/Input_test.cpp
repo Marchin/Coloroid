d29b2a4f09c4d2ae385f8509d2457eafd26da352
Marchin <mmonterrosa94@gmail.com>
2018-08-05 11:34:05 -0300

    Add Input_test

--- EngineTester/Input_test.cpp
+++ EngineTester/Input_test.cpp
@@ -1,32 +1,56 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "../SFML_Practica/Headers/Shot/ShotPool.h"
+#include "../SFML_Practica/Headers/Controllers/Input/InputObserver.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace EngineTester {
-	TEST_CLASS(ShotPool_test) {
+	static bool state;
+	bool ReadKey(int waste) {
+		return state;
+	}
+
+	TEST_CLASS(Input_test) {
 public:
 
 	TEST_METHOD(Instatiation) {
-		float deltaTime = 0.f;
-		std::vector<sf::Sprite> obstacles;
-		sf::View view(sf::FloatRect(0, 0, 256, 240));
-		shotSys::ShotPool shotPool(&view, 3);
-		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
-		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
-		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
-		Assert::IsFalse(shotPool.RequestShot(10, sf::Color::Red));
-		while (deltaTime < 5.f) {
-			shotPool.Update(deltaTime, obstacles);
-			deltaTime += .33f;
-		}
-		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
-		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
-		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
-		Assert::IsFalse(shotPool.RequestShot(10, sf::Color::Red));
+		auto a = &ReadKey;
+		state = false;
+		input::InputObserver<bool> key(a, 10);
+		Assert::IsFalse(key.IsPressed());
+		Assert::IsFalse(key.PressDown());
+		Assert::IsFalse(key.PressUp());
+		state = true;
+		key.Update();
+		Assert::IsTrue(key.IsPressed());
+		Assert::IsTrue(key.PressDown());
+		Assert::IsFalse(key.PressUp());
+		key.Update();
+		Assert::IsTrue(key.IsPressed());
+		Assert::IsFalse(key.PressDown());
+		Assert::IsFalse(key.PressUp());
+		state = false;
+		key.Update();
+		Assert::IsFalse(key.IsPressed());
+		Assert::IsFalse(key.PressDown());
+		Assert::IsTrue(key.PressUp());
+		state = false;
+		key.Update();
+		Assert::IsFalse(key.IsPressed());
+		Assert::IsFalse(key.PressDown());
+		Assert::IsFalse(key.PressUp());
 	}
+	};
 
-
+	class KeyboardMockup {
+	public:
+		KeyboardMockup() : m_state(false) {}
+		static bool ReadTrue(int a) {
+			return true;
+		}
+		bool ReadFalse() {
+			return false;
+		}
+		bool m_state;
 	};
 }

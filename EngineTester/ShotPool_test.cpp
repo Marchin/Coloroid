da6d53b2e5210ab26bf63c9fdc895cccb5755adc
Marchin <mmonterrosa94@gmail.com>
2018-08-06 02:32:38 -0300

    Update tests

--- EngineTester/ShotPool_test.cpp
+++ EngineTester/ShotPool_test.cpp
@@ -5,16 +5,16 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace EngineTester {
-	TEST_CLASS(ShotPool_test) {
+	TEST_CLASS(AsteroidPool_test) {
 public:
 
 	TEST_METHOD(Instatiation) {
 		sf::View view(sf::FloatRect(0, 0, 256, 240));
 		shotSys::ShotPool shotPool(&view, 3);
-		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
-		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
-		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
-		Assert::IsFalse(shotPool.RequestShot(10, sf::Color::Red));
+		Assert::IsTrue(shotPool.Request(10, sf::Color::Red));
+		Assert::IsTrue(shotPool.Request(10, sf::Color::Red));
+		Assert::IsTrue(shotPool.Request(10, sf::Color::Red));
+		Assert::IsFalse(shotPool.Request(10, sf::Color::Red));
 	}
 
 
@@ -23,18 +23,18 @@ public:
 		std::vector<sf::Sprite> obstacles;
 		sf::View view(sf::FloatRect(0, 0, 256, 240));
 		shotSys::ShotPool shotPool(&view, 3);
-		shotPool.RequestShot(10, sf::Color::Red);
-		shotPool.RequestShot(10, sf::Color::Red);
-		shotPool.RequestShot(10, sf::Color::Red);
-		shotPool.RequestShot(10, sf::Color::Red);
+		shotPool.Request(10, sf::Color::Red);
+		shotPool.Request(10, sf::Color::Red);
+		shotPool.Request(10, sf::Color::Red);
+		shotPool.Request(10, sf::Color::Red);
 		while (deltaTime < 5.f) {
 			shotPool.Update(deltaTime, obstacles);
 			deltaTime += .33f;
 		}
-		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
-		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
-		Assert::IsTrue(shotPool.RequestShot(10, sf::Color::Red));
-		Assert::IsFalse(shotPool.RequestShot(10, sf::Color::Red));
+		Assert::IsTrue(shotPool.Request(10, sf::Color::Red));
+		Assert::IsTrue(shotPool.Request(10, sf::Color::Red));
+		Assert::IsTrue(shotPool.Request(10, sf::Color::Red));
+		Assert::IsFalse(shotPool.Request(10, sf::Color::Red));
 	}
 
 	};

5492fbb4c85fe008abda73630e10eceb970ca81b
Marchin <mmonterrosa94@gmail.com>
2018-08-07 15:08:00 -0300

    Add constants
    ASTEROIDS
    ASTEROIDS_SPEED
    SHOTS_SPEED
    PI

--- SFML_Practica/Headers/Constants.h
+++ SFML_Practica/Headers/Constants.h
@@ -2,7 +2,11 @@
 #define CONSTANTS_H
 
 namespace constant {
-	unsigned int LIFES = 3;
+	const unsigned int LIFES = 3;
+	const unsigned int ASTEROIDS = 5;
+	const float ASTEROIDS_SPEED = 2.f;
+	const float SHOTS_SPEED = 2.f;
+	const double PI = 3.14159265;
 }
 
 #endif // !CONSTANTS_H

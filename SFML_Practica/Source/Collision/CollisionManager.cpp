5b651315a5eb6e6acfb8070d2ddf440ffb8e54c2
Marchin <mmonterrosa94@gmail.com>
2018-12-05 00:21:04 -0300

    Fix Collision Bug

--- SFML_Practica/Source/Collision/CollisionManager.cpp
+++ SFML_Practica/Source/Collision/CollisionManager.cpp
@@ -3,40 +3,40 @@
 
 namespace Collision {
 
-CollisionManager::CollisionManager(player::Player* pPlayer,
-	shotSys::ShotPool* pShots, asteroidSys::AsteroidPool* pAsteroids)
-	: m_pPlayer(pPlayer) , m_pShots(pShots), m_pAsteroids(pAsteroids)
-{}
+	CollisionManager::CollisionManager(player::Player* pPlayer,
+		shotSys::ShotPool* pShots, asteroidSys::AsteroidPool* pAsteroids)
+		: m_pPlayer(pPlayer), m_pShots(pShots), m_pAsteroids(pAsteroids) {
+	}
 
-void CollisionManager::Update() {
-	for (unsigned int iShot = 0; iShot < m_pShots->GetSize(); iShot++) {
-		if ((*m_pShots)[iShot]->IsActive()) {
-			for (unsigned int iAsteroid = 0; 
-					iAsteroid < m_pAsteroids->GetSize(); iAsteroid++) {
-				if ((*m_pAsteroids)[iAsteroid]->IsActive()) {
+	void CollisionManager::Update() {
+		for (unsigned int iAsteroid = 0;
+			iAsteroid < m_pAsteroids->GetSize(); iAsteroid++) {
+			if ((*m_pAsteroids)[iAsteroid]->IsActive()) {
 
-					if (Collision::CircleTest(m_pPlayer->GetSprite(),
-						(*m_pAsteroids)[iAsteroid]->GetSprite())) {
+				if (Collision::CircleTest(m_pPlayer->GetSprite(),
+					(*m_pAsteroids)[iAsteroid]->GetSprite())) {
 
-						(*m_pAsteroids)[iAsteroid]->Disable();
-						m_pPlayer->TakeDamage();
-					}
+					m_pPlayer->TakeDamage();
+					(*m_pAsteroids)[iAsteroid]->Disable();
+				}
+				for (unsigned int iShot = 0; iShot < m_pShots->GetSize(); iShot++) {
+					if ((*m_pShots)[iShot]->IsActive()) {
 
-					if (Collision::CircleTest((*m_pShots)[iShot]->GetSprite(),
-						(*m_pAsteroids)[iAsteroid]->GetSprite())) {
+						if (Collision::CircleTest((*m_pShots)[iShot]->GetSprite(),
+							(*m_pAsteroids)[iAsteroid]->GetSprite())) {
 
-						if ((*m_pShots)[iShot]->GetColor() == 
-							(*m_pAsteroids)[iAsteroid]->GetColor()) {
+							if ((*m_pShots)[iShot]->GetColor() ==
+								(*m_pAsteroids)[iAsteroid]->GetColor()) {
 
-							(*m_pAsteroids)[iAsteroid]->Disable();
+								(*m_pAsteroids)[iAsteroid]->Disable();
+							}
+							(*m_pShots)[iShot]->Disable();
+							break;
 						}
-						(*m_pShots)[iShot]->Disable();
-						break;
 					}
 				}
 			}
 		}
 	}
-}
 
 }
